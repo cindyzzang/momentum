88728cbc642026f94d6867cf12770972b29158d9
sinheejin <121927174+cindyzzang@users.noreply.github.com>
2023-01-09 03:14:28 +0900

    Initial commit

---
+++ js/background.js
@@ -0,0 +1,17 @@
+const images = [
+  "bgimg.jpeg",
+  "bgimg_1.jpg",
+  "bgimg_2.jpg",
+  "bgimg_3.jpg",
+  "bgimg_4.jpg",
+  "bgimg_5.jpg",
+  "bgimg_6.jpg"
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+// const bgImage = document.createElement("img");
+
+// bgImage.src = `img/${chosenImage}`;
+
+document.body.style.backgroundImage = `url(img/${chosenImage}`;
---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "f54acab6bcdde793d7c6d2e08b4c4814";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude; //위도
+  const lon = position.coords.longitude; //경도
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} ${Math.round(
+        data.main.temp
+      )}℃`;
+    });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
